43b7549d1700b3a7edc30594ea9ea2cad8bab503
汪啸 <wangxiao@changba.com>
2015-09-06 19:22:51 +0800

    bump up version

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ readme_path = path.join(here, 'README.rst')
 with copen(readme_path, encoding='utf-8') as f:
     long_description = f.read()
 
-__version__ = '4.1.1'
+__version__ = '4.1.2'
 
 setup(
     name="xUnique",
